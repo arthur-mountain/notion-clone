2350fba5ac89b881e20cacccab80bc848ecf3bf3
arthur <arthur.oceanus@gmail.com>
2023-11-19 22:18:06 +0800

    site page completed

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 import Image from 'next/image';
 
 import { cn } from '@/lib/utils';
-import { CLIENTS, USERS } from '@/constants/common';
-
-import TitleSection from '@/components/landing-page/TitleSection';
+import {
+	CLIENTS,
+	USERS,
+	PRICING_CARDS,
+	PRICING_PLANS,
+} from '@/constants/common';
+import { TitleSection, CustomCard } from '@/components/landing-page';
 import { Button } from '@/components/ui/button';
+import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
+import { CardTitle, CardDescription, CardContent } from '@/components/ui/card';
 
 const HomePage = () => {
 	return (
@@ -102,44 +108,120 @@ const HomePage = () => {
 							)}
 						>
 							{USERS.map((testimonial, index) => (
-								<div key={testimonial.name}>{testimonial.name}</div>
-								// <CustomCard
-								// 	key={testimonial.name}
-								// 	className='w-[500px]
-								//   shrink-0s
-								//   rounded-xl
-								//   dark:bg-gradient-to-t
-								//   dark:from-border dark:to-background
-								// '
-								// 	cardHeader={
-								// 		<div
-								// 			className='flex
-								//       items-center
-								//       gap-4
-								//   '
-								// 		>
-								// 			<Avatar>
-								// 				<AvatarImage src={`/avatars/${index + 1}.png`} />
-								// 				<AvatarFallback>AV</AvatarFallback>
-								// 			</Avatar>
-								// 			<div>
-								// 				<CardTitle className='text-foreground'>
-								// 					{testimonial.name}
-								// 				</CardTitle>
-								// 				<CardDescription className='dark:text-washed-purple-800'>
-								// 					{testimonial.name.toLocaleLowerCase()}
-								// 				</CardDescription>
-								// 			</div>
-								// 		</div>
-								// 	}
-								// 	cardContent={
-								// 		<p className='dark:text-washed-purple-800'>
-								// 			{testimonial.message}
-								// 		</p>
-								// 	}
-								// />
+								<CustomCard
+									key={testimonial.name}
+									className='w-[500px] shrink-0s rounded-xl dark:bg-gradient-to-t dark:from-border dark:to-background'
+									cardHeader={
+										<div className='flex items-center gap-4'>
+											<Avatar>
+												<AvatarImage src={`/avatars/${index + 1}.png`} />
+												<AvatarFallback>AV</AvatarFallback>
+											</Avatar>
+											<div>
+												<CardTitle className='text-foreground'>
+													{testimonial.name}
+												</CardTitle>
+												<CardDescription className='dark:text-washed-purple-800'>
+													{testimonial.name.toLocaleLowerCase()}
+												</CardDescription>
+											</div>
+										</div>
+									}
+									cardContent={
+										<p className='dark:text-washed-purple-800'>
+											{testimonial.message}
+										</p>
+									}
+								/>
 							))}
 						</div>
+					))}
+				</div>
+			</section>
+			<section
+				className='mt-20
+        px-4
+        sm:px-6
+      '
+			>
+				<TitleSection
+					title='The Perfect Plan For You'
+					subheading='Experience all the benefits of our platform. Select a plan that suits your needs and take your productivity to new heights.'
+					pill='Pricing'
+				/>
+				<div
+					className='flex 
+        flex-col-reverse
+        sm:flex-row
+        gap-4
+        justify-center
+        sm:items-stretch
+        items-center
+        mt-10
+        '
+				>
+					{PRICING_CARDS.map((card) => (
+						<CustomCard
+							key={card.planType}
+							className={cn(
+								'w-[300px] rounded-2xl dark:bg-black/40 background-blur-3xl relative',
+								{
+									'border-brand-primaryPurple/70':
+										card.planType === PRICING_PLANS.proplan,
+								},
+							)}
+							cardHeader={
+								<CardTitle className='text-2xl font-semibold'>
+									{card.planType === PRICING_PLANS.proplan && (
+										<>
+											<div className='hidden dark:block w-full blur-[120px] rounded-full h-32 absolute bg-brand-primaryPurple/80 -z-10 top-0' />
+											<Image
+												src='/icons/diamond.svg'
+												alt='Pro Plan Icon'
+												className='absolute top-6 right-6'
+												width={32}
+												height={32}
+											/>
+										</>
+									)}
+									{card.planType}
+								</CardTitle>
+							}
+							cardContent={
+								<CardContent className='p-0'>
+									<span className='font-normal text-2xl'>${card.price}</span>
+									{+card.price > 0 && (
+										<span className='dark:text-washed-purple-800 ml-1'>
+											/month
+										</span>
+									)}
+									<p className='dark:text-washed-purple-800'>
+										{card.description}
+									</p>
+									<Button className='whitespace-nowrap w-full mt-4'>
+										{card.planType === PRICING_PLANS.proplan
+											? 'Go Pro'
+											: 'Get Started'}
+									</Button>
+								</CardContent>
+							}
+							cardFooter={
+								<ul className='font-normal flex mb-2 flex-col gap-4'>
+									<small>{card.highlightFeature}</small>
+									{card.freatures.map((feature) => (
+										<li key={feature} className='flex items-center gap-2'>
+											<Image
+												src='/icons/check.svg'
+												alt='Check Icon'
+												width={24}
+												height={24}
+											/>
+											{feature}
+										</li>
+									))}
+								</ul>
+							}
+						/>
 					))}
 				</div>
 			</section>
