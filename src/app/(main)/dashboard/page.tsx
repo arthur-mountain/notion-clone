ba8083a265d3564119f6850b55fdae1c66243a71
arthur <arthur.oceanus@gmail.com>
2023-11-25 18:11:31 +0800

    To be continued, finish the dashboard page

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import db from '@/lib/supabase/db';
+import { createServerComponentClient } from '@/lib/supabase/utils';
 
-const DashboardPage = () => {
-  return (
-    <div>DashboardPage</div>
-  )
-}
+const DashboardPage = async () => {
+	const supabase = await createServerComponentClient();
+	const {
+		data: { user },
+	} = await supabase.auth.getUser();
 
-export default DashboardPage
+	if (!user) return;
+
+	// const workspace = await db.query.workspaces.findFirst({
+	// 	where: (workspace, { eq }) => eq(workspace.workspaceOwner, user.id),
+	// }); // yt:2:31:48
+
+	return <div>DashboardPage</div>;
+};
+
+export default DashboardPage;
