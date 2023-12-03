8d466be3b28583dcb17c7bc5fea9f6d0f9125f0e
arthur <arthur.oceanus@gmail.com>
2023-12-03 19:00:33 +0800

    Fix db migration

--- migrations/0002_wide_lucky_pierre.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "files" RENAME COLUMN "create_at" TO "created_at";--> statement-breakpoint
-ALTER TABLE "folders" RENAME COLUMN "create_at" TO "created_at";--> statement-breakpoint
-ALTER TABLE "workspaces" RENAME COLUMN "create_at" TO "created_at";
