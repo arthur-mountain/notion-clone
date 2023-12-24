669347e1dcf954a9ee2236201cd968e8c03bb1ac
arthur <arthur.oceanus@gmail.com>
2023-12-24 22:23:43 +0800

    Add logout button

---
+++ src/components/global/LogoutButton.tsx
@@ -0,0 +1,30 @@
+'use client';
+import { useRouter } from 'next/navigation';
+import { type PropsWithChildren } from 'react';
+import { createClientComponentClient } from '@/lib/supabase/utils/client';
+import { Button } from '@/components/ui/button';
+import { useAppStore } from '@/components/providers/AppProvider';
+import { cn } from '@/lib/utils';
+
+type Props = PropsWithChildren<{ className?: string }>;
+const LogoutButton = ({ children, className }: Props) => {
+	const { action } = useAppStore();
+	const router = useRouter();
+	const logout = async () => {
+		await createClientComponentClient().auth.signOut();
+		router.refresh();
+		action.setWorkspaces({ workspaces: [] });
+	};
+	return (
+		<Button
+			variant='ghost'
+			size='icon'
+			className={cn('p-0', className)}
+			onClick={logout}
+		>
+			{children}
+		</Button>
+	);
+};
+
+export default LogoutButton;
