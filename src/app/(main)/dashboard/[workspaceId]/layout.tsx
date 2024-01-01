fdbd3d41f4db4204b752f98d6d46941fbf4aa8a5
arthur <arthur.oceanus@gmail.com>
2024-01-01 21:18:50 +0800

    Rename components/sidebar with name convetion

--- src/app/(main)/dashboard/[workspaceId]/layout.tsx
+++ src/app/(main)/dashboard/[workspaceId]/layout.tsx
@@ -1,5 +1,5 @@
 import React, { type PropsWithChildren } from 'react';
-import Sidebar from '@/components/sidebar';
+import Sidebar from '@/components/Sidebar';
 
 type Props = PropsWithChildren<{ params: { workspaceId: string } }>;
 
